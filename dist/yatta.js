05c946510c420270179f72bbb7e7aeaa7c446a66
Ge Yang <yangge1987@gmail.com>
2018-01-19 17:00:29 -0600

    BUILD

--- dist/yatta.js
+++ dist/yatta.js
@@ -19,7 +19,7 @@ var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
 var search = function () {
     var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(query, options) {
-        var spinner, results, choices, exit, prompt, _ref2, selection, i, selected, fn;
+        var entry_limit, search_prompt, spinner, results, choices, exit, prompt, _ref2, selection, i, selected, fn;
 
         return _regenerator2.default.wrap(function _callee$(_context) {
             while (1) {
@@ -40,11 +40,18 @@ var search = function () {
                         return _context.abrupt("return", console.log(chalk.red('INTERNAL_ERROR: options.limit is not specified')));
 
                     case 3:
+                        entry_limit = options.limit || ENTRY_LIMIT;
+                        search_prompt = {
+                            message: "Search result from Google Scholar",
+                            type: "list",
+                            default: 0,
+                            pageSize: entry_limit + 2
+                        };
                         spinner = ora("searching google scholar for " + chalk.green(query)).start();
-                        _context.next = 6;
-                        return model.search(query, { limit: options.limit });
+                        _context.next = 8;
+                        return model.search(query, { limit: entry_limit });
 
-                    case 6:
+                    case 8:
                         results = _context.sent;
                         choices = results.map(_utils.simple);
 
@@ -57,10 +64,10 @@ var search = function () {
 
 
                         process.stdin.on('keypress', exit);
-                        _context.next = 13;
+                        _context.next = 15;
                         return prompt;
 
-                    case 13:
+                    case 15:
                         _ref2 = _context.sent;
                         selection = _ref2.selection;
                         i = choices.indexOf(selection);
@@ -89,7 +96,7 @@ var search = function () {
                         //     items: []
                         // })
 
-                    case 20:
+                    case 22:
                     case "end":
                         return _context.stop();
                 }
@@ -123,15 +130,9 @@ var model = require('./model');
 
 var EXIT_KEYS = ["escape", "q"];
 
-var ENTRIE_LIMIT = 15;
-var search_prompt = {
-    message: "Search result from Google Scholar",
-    type: "list",
-    default: 0,
-    pageSize: ENTRIE_LIMIT + 2
-};
+var ENTRY_LIMIT = 15;
 
 program.version(package_config.version).option('-d, --directory', 'the directory to apply yatta. Default to ').option('-R, --recursive', 'flag to apply yatta recursively');
 
-program.command('search <query>').option('--limit', "limit for the number of results to show on each search", parseInt, ENTRIE_LIMIT).action(search);
+program.command('search <query>').option('--limit <limit>', "limit for the number of results to show on each search", parseInt, ENTRY_LIMIT).action(search);
 program.parse(process.argv);
