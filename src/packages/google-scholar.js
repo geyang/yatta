cd6a44a6a2e7d1e09224e92a2ae38d17cdf5a6dc
Ge Yang <yangge1987@gmail.com>
2018-01-20 12:00:19 -0600

    update agent string

--- src/packages/google-scholar.js
+++ src/packages/google-scholar.js
@@ -9,7 +9,8 @@ let stripTags = require('striptags');
 const RATE_LIMITS = {S: 5, M: 200, D: 1e8};
 
 const RESULTS_PER_PAGE = 10;
-const USER_AGENT = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7';
+// const USER_AGENT = 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/604.4.7 (KHTML, like Gecko) Version/11.0.2 Safari/604.4.7';
+const USER_AGENT = 'curl/7.52.1';
 const GOOGLE_SCHOLAR_URL = 'https://scholar.google.com/scholar?hl=en&q=';
 const GOOGLE_SCHOLAR_URL_PREFIX = 'https://scholar.google.com';
 
